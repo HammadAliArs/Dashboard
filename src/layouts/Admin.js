ce2479410c13aeca939b4060c9830e622337908b
HammadAliArs <hammad47576@gmail.com>
2024-12-11 14:43:17 +0500

    Fixed HeaderStats to render only on Dashboard page

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,25 +1,25 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 
 // components
-
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 import HeaderStats from "components/Headers/HeaderStats.js";
 
 // views
-
 import Dashboard from "views/admin/Dashboard.js";
 import Settings from "views/admin/Settings.js";
 
 export default function Admin() {
+  const location = useLocation(); // Get the current route
+  
   return (
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
+        {/* Conditionally render HeaderStats */}
+        {location.pathname === "/admin/dashboard" && <HeaderStats />}
         <div className="px-1 md:px-1 mx-auto w-full m-6">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
--- src/views/admin/Settings.js
+++ src/views/admin/Settings.js
@@ -3,18 +3,15 @@ import React from "react";
 // components
 
 import CardSettings from "components/Cards/CardSettings.js";
-import CardProfile from "components/Cards/CardProfile.js";
 
 export default function Settings() {
   return (
     <>
-      <div className="flex flex-wrap">
-        <div className="w-full lg:w-8/12 px-4">
+      <div className="flex justify-center flex-wrap">
+        <div className="w-full lg:w-8/12 px-4  mt-20">
           <CardSettings />
         </div>
-        <div className="w-full lg:w-4/12 px-4">
-          <CardProfile />
-        </div>
+        
       </div>
     </>
   );
